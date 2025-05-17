c6a2e5b36ec1fef880912f7e32654ff4853a17fc
zaferatakli <zatakli@gmail.com>
2025-05-18 08:50:40 +1000

    queries13,14,15 done

--- queries/query_13.sql
+++ queries/query_13.sql
@@ -1,3 +1,20 @@
 -- 13. Find the latest salaries for each employee
 
 -- Her çalışanın en son maaşlarını bul.
+
+select * from employees.salaries;
+select * from employees.employees;
+
+select e.first_name, e.last_name, salaries.emp_no, max(salary)
+from salaries
+join employees.employees e on e.emp_no=salaries.emp_no
+group by emp_no;
+
+SELECT e.emp_no, e.first_name, e.last_name, s.salary, s.from_date
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no
+JOIN (
+    SELECT emp_no, MAX(from_date) AS latest_date
+    FROM salaries
+    GROUP BY emp_no
+) latest_s ON s.emp_no = latest_s.emp_no AND s.from_date = latest_s.latest_date;
--- queries/query_14.sql
+++ queries/query_14.sql
@@ -4,3 +4,16 @@
 --      "Satış" departmanındaki çalışanların adını, soyadını ve en yüksek maaşını listele.
 --      Listeyi en yüksek maaşa göre azalan şekilde sırala ve sadece en yüksek maaşa sahip çalışanı
 --      göster.
+
+select * from employees.employees;
+select * from employees.departments;
+select * from employees.dept_emp;
+select * from employees.salaries;
+
+select e.first_name, e.last_name, max(salary) from employees.employees e
+join employees.dept_emp de on e.emp_no = de.emp_no
+join employees.departments d on de.dept_no = d.dept_no
+join employees.salaries s on e.emp_no = s.emp_no
+where d.dept_name = 'Sales'
+group by e.emp_no
+order by max(salary) desc limit 1;
