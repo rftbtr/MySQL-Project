821e563392169e15bcbb93db39ab46c45f7b001a
yigitcam <yigitcam86@gmail.com>
2025-05-17 23:00:39 +0300

    querty 04,05,06 added

--- queries/query_04.sql
+++ queries/query_04.sql
@@ -1,3 +1,9 @@
 -- 4. Calculate the average salary of all employees with gender "M"
 
 -- "Erkek" cinsiyetindeki tüm çalışanların ortalama maaşını hesapla.
+
+select * from employees.salaries;
+select * from employees;
+select avg(s.salary) as averageSalaryMan from employees.employees e
+join employees.salaries s on e.emp_no=s.emp_no
+where e.gender='M';
